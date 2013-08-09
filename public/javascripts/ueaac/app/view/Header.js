2756ed25230f7f0cc2b0ee5067249ee2c3f05620
lzrgibson <lzrgibson@gmail.com>
2013-08-09 17:42:59 +0800

    add

---
+++ public/javascripts/ueaac/app/view/Header.js
@@ -0,0 +1,14 @@
+Ext.define('Ueaac.view.Header', {
+    extend: 'Ext.Component',
+    alias: 'widget.header',
+    dock: 'top',
+    baseCls: 'app-header-title',
+    
+    initComponent: function() {
+        Ext.applyIf(this, {
+            html: 'New UEAAC'
+        });
+                
+        this.callParent(arguments);
+    }
+});
